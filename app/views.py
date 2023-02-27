55c3486ec7e259dec8cba547dd7b1bd3bc9ae848
loganathan1831 <loga1831@gmail.com>
2023-02-27 18:19:22 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+# Create your views here.
+from app.models import *
+from django.views.generic import ListView,TemplateView,DetailView,CreateView,UpdateView,Deleteview
+from django.urls import reverse_lazy
+
+
+
+class Home(TemplateView):
+    template_name='app/home.html'
+
+class SchoolList(ListView):
+    model=School
+    context_object_name='schools'
+    #template_name='app/school_list.html'
+    #queryset=School.objects.filter(name='Jspiders')
+    ordering=['name']
+
+class SchoolDetail(DetailView):
+    model=School
+    context_object_name='sc'
+
+
+class SchoolCreate(CreateView):
+    model=School
+    fields='__all__'
+
+
+class SchoolUpdate(UpdateView):
+    model=School
+    fields='__all__'
+
+
+class SchoolDelete(Deleteview):
+    mode=School
+    context_object_name='sc'
+    success_url=reverse_lazy('SchoolList')
